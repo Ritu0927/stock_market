f05b6a9ecd20efceac33851ceb0d9c193a743ce0
Ritu0927 <159519932+Ritu0927@users.noreply.github.com>
2024-05-11 13:21:06 +0530

    Add files via upload

--- final_app(1).py
+++ final_app(1).py
@@ -159,14 +159,13 @@ plt.ylabel('Price')
 st.pyplot(fig1)
 
 st.subheader('Metrics of LSTM MODEL')
-st.subheader('Mean Squared Error of LSTM:')
-mse_lstm
-st.subheader('Accuracy of LSTM:')
-accuracy_LSTM
-st.subheader('Mean Absolute Error of LSTM:')
-mean_absolute_error_lstm
-st.subheader('Root Mean Squared Error of LSTM:')
-rmse_lstm
+st.subheader('Mean Squared Error of LSTM: '+ str(mse_lstm))
+st.subheader('Accuracy of LSTM: '+ str(accuracy_LSTM))
+
+st.subheader('Mean Absolute Error of LSTM: '+ str(mean_absolute_error_lstm))
+
+st.subheader('Root Mean Squared Error of LSTM: '+ str(rmse_lstm))
+
 print("Mean Absolute Error:",mean_absolute_error_lstm)
 print("Root Mean Squared Error:", rmse_lstm)
 
@@ -274,14 +273,263 @@ plt.ylabel('Price')
 st.pyplot(fig3)
 
 st.subheader('Metrics of Gradient Boosting Model')
-st.subheader('Mean Squared Error of GB:')
-mse_gb
-st.subheader('Accuracy of GB:')
-accuracy_gb
-st.subheader('Mean Absolute Error of GB:')
-mean_absolute_error_gb
-st.subheader('Root Mean Squared Error of GB:')
-rmse_gb
+st.subheader('Mean Squared Error of GB: '+ str(mse_gb))
+
+st.subheader('Accuracy of GB: '+ str(accuracy_gb))
+
+st.subheader('Mean Absolute Error of GB: '+ str(mean_absolute_error_gb))
+
+st.subheader('Root Mean Squared Error of GB: '+ str(rmse_gb))
+
+
+# SVR..
+
+from sklearn.svm import SVR
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+#Splitting data into training and testing
+data_training_svm = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing_svm = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))]) 
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range= (0,1))
+
+data_training_array_svm = scaler.fit_transform(data_training_svm)
+
+x_test_svm= []
+y_test_svm= []
+
+for i in range(100,data_training_array_svm.shape[0]):
+    x_test_svm.append(data_training_array_svm[i-100:i])
+    y_test_svm.append(data_training_array_svm[i,0])
+    
+x_test_svm, y_test_svm= np.array(x_test_svm),np.array(y_test_svm)
+
+from joblib import load
+
+# model_svm = load("./model_svm.joblib")
+
+model_svm = SVR(kernel='linear')
+
+x_test_svm = np.reshape(x_test_svm, (x_test_svm.shape[0], -1))
+y_test_svm = np.reshape(y_test_svm, (y_test_svm.shape[0],))
+
+model_svm.fit(x_test_svm, y_test_svm)
+
+svm_predictions = model_svm.predict(x_test_svm)
+
+fig_svm=plt.figure(figsize=(10, 6))
+st.subheader('SVM')
+plt.plot(y_test_svm, label='Actual')
+plt.plot(svm_predictions, label='Predicted')
+plt.title('Actual vs. Predicted Stock Prices')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+# plt.show()
+st.pyplot(fig_svm)
+
+def calculate_accuracy(svm_predictions, y_test_svm):
+   
+    # Calculate the absolute error between predictions and actual values
+    errors = np.abs(svm_predictions - y_test_svm)
+    
+    # Calculate the mean absolute error (MAE)
+    mean_absolute_error = np.mean(errors)
+    
+    # Calculate the accuracy
+    accuracy = 1 - (mean_absolute_error / np.mean(y_test_svm))
+    
+    return accuracy
+
+accuracy_svm = calculate_accuracy(svm_predictions, y_test_svm)
+print("Accuracy:", accuracy_svm)
+
+errors = np.abs(svm_predictions - y_test_svm)
+# Calculate the mean absolute error (MAE)
+mean_absolute_error_svm = np.mean(errors)
+print("Mean Absolute Error:",mean_absolute_error_svm)
+
+def mean_squared_error(svm_predictions, y_test_svm):
+   
+    # Calculate the squared error
+    squared_error_svm = np.square(y_test_svm - svm_predictions)
+    
+    # Calculate the mean squared error
+    mse_svm = np.mean(squared_error_svm)
+    
+    return mse_svm
+mse_svm = mean_squared_error(y_test_svm, svm_predictions)
+print("Mean Squared Error:", mse_svm)
+
+   
+    # Calculate the root mean squared error
+rmse_svm = np.sqrt(mse_svm)
+    
+print("Root Mean Squared Error:"+ str(rmse_svm))
+
+st.subheader('Metrics of SVM Model')
+st.subheader('Mean Squared Error of SVM: '+ str(mse_svm))
+
+st.subheader('Accuracy of SVM: '+ str(accuracy_svm))
+
+st.subheader('Mean Absolute Error of SVM: '+ str(mean_absolute_error_svm))
+
+st.subheader('Root Mean Squared Error of SVM: '+ str(rmse_svm))
+
+
+
+# GRU Model
+
+from keras.models import Sequential
+from keras.layers import GRU, Dense
+import matplotlib.pyplot as plt
+
+model_GRU = load_model("./GRU_model.h5")
+
+# Preprocessing data
+def preprocess_data(data):
+    scaler = MinMaxScaler(feature_range=(0,1))
+    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+    return scaled_data, scaler
+
+def create_sequences(data, seq_length):
+    x, y = [], []
+    for i in range(len(data) - seq_length):
+        x.append(data[i:(i + seq_length)])
+        y.append(data[i + seq_length])
+    return np.array(x), np.array(y)
+
+
+# Building the GRU model
+# def build_model(seq_length):
+#     model = Sequential()
+#     model.add(GRU(units=50, return_sequences=True, input_shape=(seq_length, 1)))
+#     model.add(GRU(units=50))
+#     model.add(Dense(units=1))
+#     model.compile(optimizer='adam', loss='mean_squared_error')
+#     return model
+
+# stock_data = fetch_stock_data('TSLA', '2010-01-01', '2019-12-01')    
+    # Preprocessing data
+scaled_data, scaler = preprocess_data(df)
+seq_length = 30  # Sequence length for training    
+    # Creating sequences and labels
+x, y = create_sequences(scaled_data, seq_length)
+
+ # Splitting data into training and testing sets
+split = int(0.8 * len(x))
+x_train, y_train = x[:split], y[:split]
+x_test, y_test = x[split:], y[split:]
+
+#  Reshaping data for GRU input
+x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+
+ # Building the GRU model
+# model_gru = build_model(seq_length)
+    
+    # Training the model
+model_GRU.fit(x_train, y_train, epochs=50, batch_size=32)
+
+# Making predictions
+GRU_predictions = model_GRU.predict(x_test)
+GRU_predictions = scaler.inverse_transform(GRU_predictions)
+y_test = scaler.inverse_transform(y_test.reshape(-1,1))
+
+# Plotting results
+fig_GRU=plt.figure(figsize=(12, 6))
+st.subheader('GRU')
+plt.plot(GRU_predictions, label='Predicted')
+plt.plot(y_test, label='True')
+plt.legend()
+plt.show()
+st.pyplot(fig_GRU)
+
+
+def calculate_accuracy(GRU_predictions, y_test):
+   
+    # Calculate the absolute error between predictions and actual values
+    errors = np.abs(GRU_predictions - y_test)
+    
+    # Calculate the mean absolute error (MAE)
+    mean_absolute_error = np.mean(errors)
+    
+    # Calculate the accuracy
+    accuracy = 1 - (mean_absolute_error / np.mean(y_test))
+    
+    return accuracy
+
+accuracy_gru = calculate_accuracy(GRU_predictions, y_test)
+print("Accuracy:", accuracy_gru)
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+# Evaluate the model
+def evaluate_model(y_test, GRU_predictions):
+    mae_gru = mean_absolute_error(y_test, GRU_predictions)
+    mse_gru = mean_squared_error(y_test, GRU_predictions)
+    rmse_gru = np.sqrt(mse_gru)
+    return mae_gru, mse_gru, rmse_gru
+
+# Calculate accuracy metrics
+mae_gru, mse_gru, rmse_gru = evaluate_model(y_test, GRU_predictions)
+print("Mean Absolute Error (MAE):", mae_gru)
+print("Mean Squared Error (MSE):", mse_gru)
+print("Root Mean Squared Error (RMSE):", rmse_gru)
+
+st.subheader('Metrics of GRU Model')
+st.subheader('Mean Squared Error of GRU: '+ str(mse_gru) )
+
+st.subheader('Accuracy of GRU: '+ str(accuracy_gru) )
+
+st.subheader('Mean Absolute Error of GRU: '+ str(mae_gru) )
+
+st.subheader('Root Mean Squared Error of GRU: '+ str(rmse_gru))
+
+
+
+
+import seaborn as sns
+
+# Data
+models = ['Gradient Boosting', 'LSTM', 'GRU', 'SVM']
+mse = [0.00627, 14.2066, 0.45844, 0.00335]
+accuracy = [0.9117, 0.6262, 0.9758, 0.8882]
+mae = [0.05241, 3.0479, 0.46897, 0.05018]
+rmse = [0.07918, 3.7692, 0.6771, 0.0579]
+
+# Plot
+fig_plot=plt.figure(figsize=(10, 6))
+
+# MSE
+fig_mse=plt.subplot(2, 2, 1)
+# sns.barplot(x=models, y=mse, palette='viridis')
+# plt.title('Mean Squared Error (MSE)')
+st.subheader('Mean Squared Error Comparision:')
+st.bar_chart(np.array(mse))
+
+# Accuracy
+fig_accuracy=plt.subplot(2, 2, 2)
+# sns.barplot(x=models, y=accuracy, palette='viridis')
+# plt.title('Accuracy')
+st.subheader('Accuracy Comparision:')
+st.bar_chart(np.array(accuracy))
+
+# MAE
+fig_mae=plt.subplot(2, 2, 3)
+# sns.barplot(np.array(mae))
+# plt.title('Mean Absolute Error (MAE)')
+st.subheader('Mean Absolute Error Comparision:')
+st.bar_chart(np.array(mae))
+
+# RMSE
+fig_rmse=plt.subplot(2, 2, 4)
+# sns.barplot(np.array(mae))
+# plt.title('Root Mean Squared Error (RMSE)')
+st.subheader('Root Mean Squared Error Comparision:')
+st.bar_chart(np.array(rmse))
 
 
 
